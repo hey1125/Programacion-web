7b3fca3aab27c43082303dc3cd97ccef95c9334a
Daniel2511 <valeriocdaniel@gmail.com>
2026-03-08 04:33:33 -0600

    cambias de referencias

--- js/header_loader.js
+++ js/header_loader.js
@@ -15,3 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error("No se encontró un elemento con el ID 'header-placeholder' en el HTML.");
     }
 });
+
+function toggleMenu() {
+    const menu = document.getElementById("dropdownMenu");
+    menu.classList.toggle("active");
+}
+
+// cerrar menú si se hace clic fuera
+window.addEventListener("click", function(event) {
+    const avatar = document.querySelector(".avatar");
+    const menu = document.getElementById("dropdownMenu");
+
+    if (!avatar.contains(event.target) && !menu.contains(event.target)) {
+        menu.classList.remove("active");
+    }
+});
