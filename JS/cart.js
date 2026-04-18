c93f31f6a4f1362e281752b9db645d3bfaf9edac
Andres Alvarez Borbon <aalvarezb@ucenfotec.ac.cr>
2026-04-17 19:58:03 -0600

    Habilitar creación de ordenes

--- JS/cart.js
+++ JS/cart.js
@@ -71,12 +71,75 @@ document.addEventListener("DOMContentLoaded", () => {
 
     });
 
-    //Redirección confirmar orden
     document.getElementById("confirm-order")
-        .addEventListener("click", () => {
+    .addEventListener("click", async () => {
+
+        //Validar campos
+        const name = document.getElementById("name").value;
+        const lastname = document.getElementById("lastname").value;
+        const address = document.getElementById("address").value;
+        const city = document.getElementById("city").value;
+        const province = document.getElementById("province").value;
+        const zip = document.getElementById("zip").value;
+
+        if (!name || !lastname || !address || !city || !province || !zip) {
+            alert("Completa toda la información de envío");
+            return;
+        }
+
+        const paymentMethodInput = document.querySelector("input[name='payment']:checked");
+
+        if (!paymentMethodInput) {
+            alert("Selecciona un método de pago");
+            return;
+        }
+
+        const paymentMethod = paymentMethodInput.value;
+
+        //Total
+        const total = parseFloat(
+            document.getElementById("total").textContent.replace("$", "")
+        );
+
+        const orderData = {
+            shippingAddress: {
+                street: address,
+                city: city,
+                zip: zip,
+                country: "Costa Rica"
+            },
+            paymentMethod,
+        };
+
+        console.log(orderData);
+
+        try {
+            const token = localStorage.getItem("token");
+
+            const res = await fetch("http://localhost:5000/api/orders", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify(orderData)
+            });
+
+            const data = await res.json();
+
+            if (!res.ok) {
+                alert(data.message || "Error al crear la orden");
+                return;
+            }
+
+            alert("Pedido creado correctamente");
 
             window.location.href = "orders.html";
 
-        });
+        } catch (error) {
+            console.error(error);
+            alert("Error de conexión con el servidor");
+        }
+    }); 
 
 });
